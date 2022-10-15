53af6adc76b2c2de138f4177de378e01f918defa
tavs <82376053+7iquid@users.noreply.github.com>
2022-10-16 03:18:40 +0800

    add sequelize

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var joborder       = require('.././models').joborder;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/api', function(req, res, next) {
+  console.log('=============>', joborder)
   res.render('index', { title: 'Express' });
 });
 
