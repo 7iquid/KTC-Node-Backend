1d535b243d983c9bb365a0fc8cfbc9138f8669ce
tavs <82376053+7iquid@users.noreply.github.com>
2022-10-19 10:07:41 +0800

    Study bytea

--- models/joborder.js
+++ models/joborder.js
@@ -17,7 +17,8 @@ module.exports = (sequelize, DataTypes) => {
     JoNumber: {
       type: DataTypes.STRING,
       allowNull: false
-    }
+    },
+
   }, {
     sequelize,
     modelName: 'JobOrder',
