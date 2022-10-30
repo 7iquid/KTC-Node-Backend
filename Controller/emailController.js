b4a5b8766c08479a625f726ff8e0e61a66269567
tavs <82376053+7iquid@users.noreply.github.com>
2022-10-30 11:06:13 +0800

    working program for nodemailer

--- Controller/emailController.js
+++ Controller/emailController.js
@@ -4,10 +4,13 @@ const {mailOptions , transport, HtmlToSend} = require('./emailTemplate/mailerKo'
 
 const emailControl = {
   async create({ body}, res, next) {
+    if (body.email || body.message){res.sendStatus(400).end({email:undefined,message:undefined})}
+
     try{
+      // data checkert
       // const {email, name, message, } = body
-      console.log(HtmlToSend(body))
-      transport.sendMail(mailOptions(body), function(error, info){
+      // console.log(HtmlToSend(body))
+      transport.sendMail(mailOptions(body, await HtmlToSend(body)), function(error, info){
           if (error) {
 
             return res.status(400).send({error: error})
