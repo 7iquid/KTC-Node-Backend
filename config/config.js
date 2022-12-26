6b902c0d5b081d4bdd33b63ce9fc90acee6256c1
tavs <82376053+7iquid@users.noreply.github.com>
2022-12-27 05:05:46 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -23,12 +23,12 @@ module.exports =
     "EMIL_PASSWORD": "gsqdjhdwyoelcwkx"
   },
   "production": {
-    "username":  "postgres",
-    "password":  "PW6acPtyfNQokcqAeBcH",
-    "database":  "railway",
-    "host":     "containers-us-west-77.railway.app",
+    "username":  "tamina",
+    "password":  "GFIBKE6zWZVdn9QINNowZBZO9eGxBvJf",
+    "database":  "tamina",
+    "host":     "dpg-cel0nmpa6gdkdn1aqts0-a",
     "dialect":  "postgres",
-    "port":      "6404",
+    "port":      "5432",
     "EMIL_USERNAME": "tavlegktc@gmail.com",
     "EMIL_PASSWORD": "gsqdjhdwyoelcwkx"
   },
