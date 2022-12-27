0cf9aff93407055310ea05b0e8b495ff66c06145
tavs <82376053+7iquid@users.noreply.github.com>
2022-12-27 12:58:49 +0800

    Revert "Update config.js"
    This reverts commit a2790822e9316f12540d6c83953824d18f2579be.

--- config/config.js
+++ config/config.js
@@ -4,11 +4,11 @@ module.exports =
 {
   "development": {
     "username":  "postgres",
-    "password":  "GFIBKE6zWZVdn9QINNowZBZO9eGxBvJf",
-    "database":  "tamina",
-    "host":     "dpg-cel0nmpa6gdkdn1aqts0-a.render.app",
+    "password":  "PW6acPtyfNQokcqAeBcH",
+    "database":  "railway",
+    "host":     "containers-us-west-77.railway.app",
     "dialect":  "postgres",
-    "port":      "5432",
+    "port":      "6404",
     "EMIL_USERNAME": "tavlegktc@gmail.com",
     "EMIL_PASSWORD": "gsqdjhdwyoelcwkx"
   },
