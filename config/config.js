3c3484c42feffac987d45046dd45a74448f7c9fb
tavs <82376053+7iquid@users.noreply.github.com>
2022-12-27 13:05:52 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,12 +3,12 @@ require('dotenv').config({ path: require('find-config')('.env') })
 module.exports = 
 {
   "development": {
-    "username":  "postgres",
-    "password":  "PW6acPtyfNQokcqAeBcH",
-    "database":  "railway",
-    "host":     "containers-us-west-77.railway.app",
+    "username":  "tamina",
+    "password":  "GFIBKE6zWZVdn9QINNowZBZO9eGxBvJf",
+    "database":  "tamina",
+    "host":     "dpg-cel0nmpa6gdkdn1aqts0-a",
     "dialect":  "postgres",
-    "port":      "6404",
+    "port":      "5432",
     "EMIL_USERNAME": "tavlegktc@gmail.com",
     "EMIL_PASSWORD": "gsqdjhdwyoelcwkx"
   },
